01c508b0aa5c5558132c61673eb3ab4cd614fc16
Mr.Papa <jakubhulin30@gmail.com>
2024-12-14 20:51:46 +0100

    backend apky, graf

--- login.py
+++ login.py
@@ -4,23 +4,21 @@ import json
 
 app = ctk.CTk()
 app.title("PyDex Login")
-app.geometry("300x500")
+app.geometry("320x240")
+
 
 
 
 ctk.set_default_color_theme("blue")
-
-#def trying(username, password):
-    
 
 def login():
     username = username_entry.get()
     password = password_entry.get()
     print(username, password)
     try:
-        dexcom = Dexcom(username= username, password= password, region="ous")
+        dexcom = Dexcom(username=username, password=password, region="ous")
         get_reading = dexcom.get_current_glucose_reading()
-        if(get_reading != None):
+        if get_reading != None:
             print(get_reading.mmol_l)
             reading = get_reading.mmol_l
             print(reading)
@@ -29,32 +27,29 @@ def login():
             test = False
     except:
         test = False
-    if test == True:
+    if test:
         status_label.configure(text=f"Login successful, {get_reading.mmol_l} mmol/L")
-        #runnovanie appky
+        # runnovanie appky
     else:
         status_label.configure(text="Login failed")
 
 
+frame = ctk.CTkFrame(master=app, width=300, height=200, corner_radius=15)
+frame.pack(pady=20, padx=10, fill="both", expand=True)
 
-frame = ctk.CTkFrame(master=app, width=300, height=400, corner_radius=15)
-frame.pack(pady=50, padx=20, fill="both", expand=True)
-
-
-login_label = ctk.CTkLabel(master=frame, text="Login", font=("Arial", 24))
-login_label.pack(pady=20)
+login_label = ctk.CTkLabel(master=frame, text="Login", font=("Arial", 18))
+login_label.pack(pady=10)
 
 username_entry = ctk.CTkEntry(master=frame, placeholder_text="Username")
-username_entry.pack(pady=10, padx=20)
+username_entry.pack(pady=5, padx=10)
 
 password_entry = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
-password_entry.pack(pady=10, padx=20)
+password_entry.pack(pady=5, padx=10)
 
 login_button = ctk.CTkButton(master=frame, text="Login", command=login)
-login_button.pack(pady=20)
+login_button.pack(pady=10)
 
-status_label = ctk.CTkLabel(master=frame, text="", font=("Arial", 12))
-status_label.pack(pady=10)
-
+status_label = ctk.CTkLabel(master=frame, text="", font=("Arial", 10))
+status_label.pack(pady=5)
 
 app.mainloop()
